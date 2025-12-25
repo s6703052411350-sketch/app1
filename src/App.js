9e5c34b8a74e784495b297dec4d42175f679b2e9
JAKPAT <s6703052411350@email.kmutnb.ac.th>
2025-12-25 11:11:39 +0700

    Function Components
    Function Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { Header } from './func-components';
+import { Content, Footer, Header } from './func-components';
 
  
 function App() {
 return (
-  [<Header/>]
+  [<Header/>,<Content/>,<Footer/>]
 )
  
 }
--- src/func-components.js
+++ src/func-components.js
@@ -15,3 +15,18 @@ export function Header(){
         </div>
     )
 }
+export function Content(){
+    return(
+        <div style={{textAlign:'center'}}>
+            <h2>Hello <br/> React & React Native</h2>
+            <br/><br/><br/>
+        </div>
+    )
+}
+export const Footer=()=>{
+    return(
+        <div style={{textAlign:'center'}}>
+            &copy; {new Date().getFullYear()} All right reserved
+        </div>
+    )
+}
