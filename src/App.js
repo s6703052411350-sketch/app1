51608b4a5c449471c1e38be9aa5cc3effe921489
s6703052411350-sketch <s6703052411350@email.kmutnb.ac.th>
2026-01-08 12:33:28 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Button from './class-components';
- 
+import {Calculator2} from './Calculator';
+
 function App() {
-  return <Button/>
+  return <Calculator2/>
 }
  
 export default App;
