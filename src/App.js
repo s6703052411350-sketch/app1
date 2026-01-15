e6857e55965b6ca656df8e35f1a67ab6e6f2c36f
s6703052411350-sketch <s6703052411350@email.kmutnb.ac.th>
2026-01-15 11:18:44 +0700

    MessageBox
    MessageBox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import EventData2 from './even-data';
 import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 function App() {
-  return <RefsArray/>
+  return <MessageBox/>
 }
 export default App;
