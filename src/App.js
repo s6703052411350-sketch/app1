2c3c77577f61c954d74ff763eb19743c7a67855c
s6703052411350-sketch <s6703052411350@email.kmutnb.ac.th>
2026-01-15 10:36:48 +0700

    RefsArray
    RefsArray

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import EventData2 from './even-data';
+import RefsArray from './refs-array';
 
 function App() {
-  return <EventData2/>
+  return <RefsArray/>
 }
- 
 export default App;
