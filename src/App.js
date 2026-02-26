2d95144f4a0b9ea4522f285e7636c23e4d8ca2c7
s6703052411350-sketch <s6703052411350@email.kmutnb.ac.th>
2026-02-26 10:29:53 +0700

    SelectFile

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Content2 from './context-content2'
 import Router2 from './router2'
 import Router3 from './router3'
 import FormText from './form-text'
+import FormSelectFile from './form-select-file'
 
-function App() {
-  return <FormText/>
+export default function App() {
+  return <FormSelectFile/>;
 }
-export default App;
